87be599f1fbc57c5a03399d0194084a8d9adda05
Benis Tambe <will.jenifer@yahoo.com>
2021-03-29 22:22:56 -0400

    commit tree 4c47a633046f5022df9890c30bcf79e56463c8ec

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find (the_id, db)
+        sql = <<-SQL
+            SELECT * FROM pokemon WHERE id = ? 
+        SQL
+
+        pokemon = db.execute(sql, the_id).flatten
+        Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    end
+
 end
